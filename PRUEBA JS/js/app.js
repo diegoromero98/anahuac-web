f2689ee9fb4a45e277b1d6f2aa4c772ca87cc46d
diegoromero98 <59923916+diegoromero98@users.noreply.github.com>
2020-03-25 08:55:09 -0600

    Prueba

---
+++ PRUEBA JS/js/app.js
@@ -0,0 +1,12 @@
+VANTA.TRUNK({
+    el: "#your-element-selector",
+    mouseControls: true,
+    touchControls: true,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xff8700,
+    backgroundColor: 0xffffff,
+    chaos: 4.50
+  })
